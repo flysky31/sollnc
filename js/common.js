eb8ee90d6f16152af6e661ccb05a87d0c4f8ffce
LeeJaMin <sweetmin33@naver.com>
2024-08-30 14:44:02 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     //mobile menu 
     $('.hamburger-menu').click(function() {
@@ -14,34 +13,7 @@ $(document).ready(function(){
             }).mouseleave(function () {      
                 $("body").removeClass("hovered");  
             });
-
-             $(".main_visual .slide02").mouseenter(function () {       
-                $("body").addClass("hovered2");       
-            }).mouseleave(function () {      
-                $("body").removeClass("hovered2");  
-            });
-            $(".main_visual .slide03").mouseenter(function () {       
-                $("body").addClass("hovered3");       
-            }).mouseleave(function () {      
-                $("body").removeClass("hovered3");  
-            });
-            $(".main_visual .slide04").mouseenter(function () {       
-                $("body").addClass("hovered4");       
-            }).mouseleave(function () {      
-                $("body").removeClass("hovered4");  
-            });
-            $(".main_visual .slide05").mouseenter(function () {       
-                $("body").addClass("hovered5");       
-            }).mouseleave(function () {      
-                $("body").removeClass("hovered5");  
-            }); 
-        } else{
-            $(".main_visual .slide01").off("mouseenter mouseleave");
-            $(".main_visual .slide02").off("mouseenter mouseleave");
-            $(".main_visual .slide03").off("mouseenter mouseleave");
-            $(".main_visual .slide04").off("mouseenter mouseleave");
-            $(".main_visual .slide05").off("mouseenter mouseleave");
-        }
+        } 
     }
 
      applyHoverEffect(); 
@@ -52,22 +24,6 @@ $(document).ready(function(){
 
 
 
-
-
-
-    // //硫붿씤 鍮꾨뵒�� opacity
-    // $(window).on('scroll', function(){
-    //     //let _offMain = $("main").offset().top;
-    //     //console.log(_offMain);      
-      
-    //     if($(this).scrollTop() <= 1000){                
-    //         $(".main_video").css({
-    //             "opacity" : ( 1 - $(window).scrollTop() /1000) 
-    //         });           
-    //     } 
-    // });
-
-    //�ㅻ뜑 fix
     $(window).scroll(function(){
         let rollTop = $(window).scrollTop();
         if(rollTop >= 50){
@@ -77,6 +33,4 @@ $(document).ready(function(){
         }
     });
 
-
-
-})
+});
